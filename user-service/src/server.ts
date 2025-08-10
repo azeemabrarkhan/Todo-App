ac8519d932766e11b5996d0c0c966ce953af95fc
azeemabrarkhan <97536297+azeemabrarkhan@users.noreply.github.com>
2025-08-10 07:07:22 +0200

    update server to include the userRouter and use new database based on Sequelize

--- user-service/src/server.ts
+++ user-service/src/server.ts
@@ -1,9 +1,19 @@
 import express from "express";
-import registerRouter from "./routes/register.js";
+import database from "./db.js";
+import userRouter from "./routes/user.js";
 
 const PORT = 4000;
 const app = express();
 
 app.use(express.json());
-app.use("/register", registerRouter);
-app.listen(PORT, () => console.log(`Server has started on port: ${PORT}`));
+app.use("/api/user", userRouter);
+
+if (database !== undefined) {
+  database
+    .sync()
+    .then(() => {
+      console.log("Database connected & models synced");
+      app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    })
+    .catch((err) => console.error("DB connection error:", err));
+}

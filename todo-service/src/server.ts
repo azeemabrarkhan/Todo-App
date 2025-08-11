15bac757262afbc2496e67e7b2b7ad0122e43a27
azeemabrarkhan <97536297+azeemabrarkhan@users.noreply.github.com>
2025-08-11 16:38:42 +0200

    refactor service servers for better readability

--- todo-service/src/server.ts
+++ todo-service/src/server.ts
@@ -11,11 +11,14 @@ app.use("/api/todo", todoRouter);
 app.use(handleError);
 
 if (database !== undefined) {
-  database
-    .sync()
-    .then(() => {
-      console.info("Todo database connected & models synced");
-      app.listen(CONSTANTS.API_PORT, () => console.info(`Todo server running on port ${CONSTANTS.API_PORT}`));
-    })
-    .catch((err) => console.error("Todo DB connection error:", err));
+  try {
+    await database.authenticate();
+    await database.sync();
+
+    console.info("Todo database connected & models synced");
+    app.listen(CONSTANTS.API_PORT, () => console.info(`Todo server running on port ${CONSTANTS.API_PORT}`));
+  } catch (err) {
+    console.error("Todo DB connection error:", err);
+    process.exit(1);
+  }
 }
--- user-service/src/server.ts
+++ user-service/src/server.ts
@@ -11,11 +11,14 @@ app.use("/api/user", userRouter);
 app.use(handleError);
 
 if (database !== undefined) {
-  database
-    .sync()
-    .then(() => {
-      console.info("User database connected & models synced");
-      app.listen(CONSTANTS.API_PORT, () => console.info(`User server running on port ${CONSTANTS.API_PORT}`));
-    })
-    .catch((err) => console.error("User DB connection error:", err));
+  try {
+    await database.authenticate();
+    await database.sync();
+
+    console.info("User database connected & models synced");
+    app.listen(CONSTANTS.API_PORT, () => console.info(`User server running on port ${CONSTANTS.API_PORT}`));
+  } catch (err) {
+    console.error("User DB connection error:", err);
+    process.exit(1);
+  }
 }

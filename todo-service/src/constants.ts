49b94417dd40ae394cb0fe7398c2ffeb925cf222
azeemabrarkhan <97536297+azeemabrarkhan@users.noreply.github.com>
2025-08-12 08:38:06 +0200

    set CORS for the todo service

--- todo-service/src/constants.ts
+++ todo-service/src/constants.ts
@@ -2,4 +2,5 @@ export const CONSTANTS = {
   DB_PORT: 5432,
   API_PORT: 5000,
   MIN_PASSWORD_LENGTH: 6,
+  FRONTEND_ADDRESS: "http://localhost:5173",
 };
--- todo-service/src/server.ts
+++ todo-service/src/server.ts
@@ -1,10 +1,19 @@
 import express from "express";
+import cors from "cors";
 import database from "./db.js";
 import todoRouter from "./routes/todo.js";
 import { CONSTANTS } from "./constants.js";
 import { handleError } from "./middlewares/error.js";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: CONSTANTS.FRONTEND_ADDRESS,
+    methods: ["GET", "PATCH", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use("/api/todo", todoRouter);
